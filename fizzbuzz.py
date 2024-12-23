378f0de806b2a2e3be1a51eb3348bc65b77c6e14
Julio Francisco Zerpa Mayor <35926348+FranciscoZ360@users.noreply.github.com>
2024-12-23 11:56:23 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
+#Primero se establece el rango de numeros
 for i in range(1, 101):
+#Usamos la condicionales de if para verificar si cumple las condiciones que queremos.
     if i % 3 == 0:
         print("fizz")
     if i % 5 == 0:
         print("buzz")
     if i % 5 == 0:
         print("buzz")
+#usamos el "AND" dentro de la condicional para que nos imprima si se cumple las dos condicionales a la vez
     if i % 3 != 0 and i % 5 != 0:
         print("fizzbuzz")
     else:
